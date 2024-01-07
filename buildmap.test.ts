00a0c9b7372fb3f7cea4131a29d923fac74c4253
Andre Eickler <797483+eickler@users.noreply.github.com>
2024-01-07 19:48:02 +0100

    Added logic and a few test cases

---
+++ buildmap.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, it } from "vitest";
+import { processVocab, parse } from "./buildmap";
+
+describe("processEntries", () => {
+  it("should filter out non-vocabulary entries", () => {
+    const result = new Map<string, number>();
+    const entries = [
+      { object: "vocabulary", id: 1, data: { characters: "一" } },
+      { object: "kanji", id: 3, data: { characters: "三" } },
+    ];
+
+    processVocab(entries, result);
+
+    console.log(result);
+    expect(result.size).toEqual(1);
+    expect(result.get("一")).toEqual(1);
+  });
+
+  it("should map the entries to the expected format", () => {
+    const result = new Map<string, number>();
+    const entries = [
+      { object: "vocabulary", id: 1, data: { characters: "一" } },
+      { object: "vocabulary", id: 2, data: { characters: "二" } },
+    ];
+    processVocab(entries, result);
+
+    expect(result.size).toEqual(2);
+    expect(result.get("一")).toEqual(1);
+    expect(result.get("二")).toEqual(2);
+  });
+});
+
+describe("parse", () => {
+  it("should parse a standard edict2 line", () => {
+    const dictionaryMap = new Map<string, string[]>();
+    const line =
+      "訳合い;訳合 [わけあい] /Logik einer Angelegenheit/Grund/Umstände/Bedeutung/";
+
+    parse(line, dictionaryMap);
+
+    expect(dictionaryMap.size).toEqual(2);
+    expect(dictionaryMap.get("訳合")).toEqual(dictionaryMap.get("訳合"));
+    expect(dictionaryMap.get("訳合い")).toEqual([
+      "Logik einer Angelegenheit",
+      "Grund",
+      "Umstände",
+      "Bedeutung",
+    ]);
+  });
+
+  it("should parse a standard edict2 line with usage hint", () => {
+    const dictionaryMap = new Map<string, string[]>();
+    const line =
+      " …倒れ […だおれ] an Ren’yōkei eines Verbes/durch den vorstehenden Vorgang sein Vermögen verlieren/etw., das besser aussieht, als es in Wirklichkeit ist/";
+
+    parse(line, dictionaryMap);
+
+    expect(dictionaryMap.size).toEqual(1);
+    expect(dictionaryMap.get("…倒れ")).toBeDefined();
+    expect(dictionaryMap.get("…倒れ")?.length).toEqual(2);
+    expect(dictionaryMap.get("…倒れ")?.[0]).toEqual(
+      expect.stringContaining("durch den vorstehenden")
+    );
+  });
+});
---
+++ buildmap.ts
@@ -0,0 +1,107 @@
+#!/usr/bin/env ts-node
+
+import axios from "axios";
+import * as fs from "fs";
+
+const token = process.env.API_TOKEN;
+
+const sleep = (milliseconds: number) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+export interface VocabularyEntry {
+  object: string;
+  data: {
+    characters: string;
+  };
+  id: number;
+}
+
+export const processVocab = (
+  entries: VocabularyEntry[],
+  vocab: Map<string, number>
+) => {
+  entries.forEach((element) => {
+    if (element.object === "vocabulary") {
+      vocab.set(element.data.characters, element.id);
+    }
+  });
+};
+
+const fetchData = async () => {
+  let nextUrl = "https://api.wanikani.com/v2/subjects";
+  const vocab = new Map<string, number>();
+
+  while (nextUrl) {
+    const response = await axios.get(nextUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    processVocab(response.data.data, vocab);
+    nextUrl = response.data.pages.next_url;
+    await sleep(1000); // be kind to the server
+  }
+  return vocab;
+};
+
+export const parse = (line: string, dictionary: Map<string, string[]>) => {
+  const match = /\s*(.+) \[[^/]+\/(.*)\//.exec(line);
+  if (match) {
+    const [, japaneseWords, germanTranslations] = match;
+    const japaneseWordList = japaneseWords.split(";");
+
+    japaneseWordList.forEach((japaneseWord) => {
+      const germanMeanings = germanTranslations.split("/");
+      const existingMeanings = dictionary.get(japaneseWord) || [];
+      dictionary.set(japaneseWord, [...existingMeanings, ...germanMeanings]);
+    });
+  }
+  // There are numerous entries in the original file that are not matching, but these are broken.
+};
+
+const readDictionaryFile = (filePath: string): Map<string, string[]> => {
+  const dictionary = new Map<string, string[]>();
+
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const lines = fileContent.split("\n");
+
+  lines.forEach((line) => parse(line, dictionary));
+  return dictionary;
+};
+
+const buildTranslations = (
+  dictionary: Map<string, string[]>,
+  vocab: Map<string, number>
+) => {
+  const translations = new Map<number, string[]>();
+  const untranslated: string[] = [];
+
+  vocab.forEach((id, word) => {
+    const meanings = dictionary.get(word);
+    if (meanings) {
+      translations.set(id, meanings);
+    } else {
+      untranslated.push(word);
+    }
+  });
+
+  return { translations, untranslated };
+};
+
+export const buildMap = async () => {
+  const dictionary = readDictionaryFile("wadokudict2");
+  const vocab = await fetchData();
+  const { translations, untranslated } = buildTranslations(dictionary, vocab);
+
+  const translationsJSON = JSON.stringify(translations);
+  fs.writeFileSync("translations.json", translationsJSON);
+
+  const untranslatedJSON = JSON.stringify(untranslated);
+  fs.writeFileSync("misses.json", untranslatedJSON);
+};
+
+if (token) {
+  buildMap();
+}
